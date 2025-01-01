13cae4efb2762a2e4b118987cc29d1bb509a9e63
RicardoAyala2003 <92557271+RicardoAyala2003@users.noreply.github.com>
2025-01-01 17:43:54 -0600

    orar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NAVBAR from "../components/Navbar/Navbar.jsx";
 import Inicio from "../pages/Inicio/Inicio.jsx";
 import Registro from "../pages/Inicio/Formularios/Registro.jsx";
+import Orar from "../pages/Inicio/Formularios/Orar.jsx";
 import Ministerio from "../pages/Ministerio/Ministerio.jsx";
 import Multimedia from "../pages/Multimedia/Multimedia.jsx";
 import Aportaciones from "../pages/Aportaciones/Aportaciones.jsx";
@@ -17,6 +18,7 @@ function App() {
           <Route path="/ministerio" element={<Ministerio />} />
           <Route path="/multimedia" element={<Multimedia />} />
           <Route path="/aportaciones" element={<Aportaciones />} />
+          <Route path="/orar" element={<Orar />} />
           <Route path="/envivo" element={<Envivo />} />
         </Routes>
       </NAVBAR>
