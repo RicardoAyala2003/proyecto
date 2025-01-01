bfa307980c4e9d0c6b4ed4ae9178c37d7e1e6d91
josueromero2002 <josuepinro21@unitec.edu>
2025-01-01 12:55:33 -0600

    Tailwind Base

--- frontend/pages/Inicio/Inicio.jsx
+++ frontend/pages/Inicio/Inicio.jsx
@@ -5,6 +5,7 @@ const Inicio = () => {
     <div>
       <h1>Bienvenido a la Página de Inicio</h1>
       <p>Esta es la página de inicio de nuestro proyecto.</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
