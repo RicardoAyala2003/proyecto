cf149cb18bc8b1768f9496d31ee32f155ae55ea8
josueromero2002 <josuepinro21@unitec.edu>
2025-01-20 21:50:04 -0600

    fix: update video sources and clean up EnVivo component

--- frontend/pages/En_Vivo/EnVivo.jsx
+++ frontend/pages/En_Vivo/EnVivo.jsx
@@ -3,10 +3,29 @@ import React, { useState } from "react";
 const EnVivo = () => {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 
+  // <iframe
+  //   width="687"
+  //   height="515"
+  //   src="https://www.youtube.com/embed/-pl3VFsjcrI"
+  //   title="Te Alabaré Mi Buen Jesús - Danilo Montero (LETRA)"
+  //   frameborder="0"
+  //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  //   referrerpolicy="strict-origin-when-cross-origin"
+  //   allowfullscreen></iframe>;
+  // <iframe
+  //   width="687"
+  //   height="515"
+  //   src="https://www.youtube.com/embed/Z5UiBleDnhY"
+  //   title="Eres todo poderoso con letra[Danilo Montero]"
+  //   frameborder="0"
+  //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  //   referrerpolicy="strict-origin-when-cross-origin"
+  //   allowfullscreen></iframe>;
+
   const videos = [
-    "https://www.youtube.com/embed/oRdxUFDoQe0?si=DRnsSALTHvxJobiS",
-    "https://www.youtube.com/embed/Zi_XLOBDo_Y?si=yPE0rEIF9Ly_h6EX",
-    "https://www.youtube.com/embed/h_D3VFfhvs4?si=ekoCtWN9dls8E4ik",
+    "https://www.youtube.com/embed/-pl3VFsjcrI",
+    "https://www.youtube.com/embed/Roo4uEQzJms",
+    "https://www.youtube.com/embed/Z5UiBleDnhY",
   ];
 
   const nextVideo = () => {
@@ -25,9 +44,7 @@ const EnVivo = () => {
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-b from-blue-100 to-white px-4">
       <div className="bg-white rounded-lg shadow-lg w-full max-w-7xl p-4 md:p-6">
         {/* Title */}
-        <h2 className="text-xl md:text-3xl font-bold text-gray-800 text-center mb-4 md:mb-6">
-
-        </h2>
+        <h2 className="text-xl md:text-3xl font-bold text-gray-800 text-center mb-4 md:mb-6"></h2>
 
         {/* Video Player */}
         <div className="flex flex-col items-center">
@@ -38,8 +55,7 @@ const EnVivo = () => {
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
+              allowFullScreen></iframe>
           </div>
 
           {/* Navigation Buttons */}
@@ -47,8 +63,7 @@ const EnVivo = () => {
             {/* Left Button */}
             <button
               onClick={prevVideo}
-              className="flex items-center bg-gray-800 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-gray-600 transition-all duration-200"
-            >
+              className="flex items-center bg-gray-800 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-gray-600 transition-all duration-200">
               <span className="mr-2">&#8592;</span>
               <span className="text-sm md:text-base font-bold">Anterior</span>
             </button>
@@ -56,8 +71,7 @@ const EnVivo = () => {
             {/* Right Button */}
             <button
               onClick={nextVideo}
-              className="flex items-center bg-gray-800 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-gray-600 transition-all duration-200 ml-4"
-            >
+              className="flex items-center bg-gray-800 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-gray-600 transition-all duration-200 ml-4">
               <span className="text-sm md:text-base font-bold">Siguiente</span>
               <span className="ml-2">&#8594;</span>
             </button>
@@ -71,8 +85,7 @@ const EnVivo = () => {
               key={index}
               className={`h-3 w-3 rounded-full ${
                 index === currentVideoIndex ? "bg-gray-800" : "bg-gray-400"
-              }`}
-            ></div>
+              }`}></div>
           ))}
         </div>
       </div>
